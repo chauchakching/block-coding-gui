4933892c8fca8dacbe8f4a0ed3c1ffb0ded8c278
chauchakching <chauchakching@gmail.com>
2020-03-19 23:21:27 +0800

    refactor: refactor similar props

--- src/helper.js
+++ src/helper.js
@@ -6,13 +6,20 @@ import HelloWorldBlock from './components/HelloWorldBlock'
 import ForLoopBlock from './components/ForLoopBlock'
 
 export const nodeTree2rTree = ({ nodeTree, dispatch }) =>
-  nodeTree.map(val => ({
-    ...val,
-    title:
-      val.instruction === INSTRUCTION.HELLO_WORLD ? (
-        <HelloWorldBlock dispatch={dispatch} nodeKey={val.key} {...val} />
-      ) : val.instruction === INSTRUCTION.FOR_LOOP ? (
-        <ForLoopBlock dispatch={dispatch} nodeKey={val.key} {...val} />
-      ) : null,
-    children: nodeTree2rTree({ nodeTree: val.children || [], dispatch }),
-  }))
+  nodeTree.map(val => {
+    const props = {
+      dispatch,
+      nodeKey: val.key,
+      ...val,
+    }
+    return {
+      ...val,
+      title:
+        val.instruction === INSTRUCTION.HELLO_WORLD ? (
+          <HelloWorldBlock {...props} />
+        ) : val.instruction === INSTRUCTION.FOR_LOOP ? (
+          <ForLoopBlock {...props} />
+        ) : null,
+      children: nodeTree2rTree({ nodeTree: val.children || [], dispatch }),
+    }
+  })
